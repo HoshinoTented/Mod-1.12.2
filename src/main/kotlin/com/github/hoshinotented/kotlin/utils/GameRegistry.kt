6e49c76a84c6095cd9baedff621b205d0619f557
Hoshino Tented <limbolrain@gmail.com>
2018-05-13 18:00:29 +0800

    移动一些奇怪的东西

--- src/main/kotlin/com/github/hoshinotented/kotlin/utils/GameRegistry.kt
+++ src/main/kotlin/com/github/hoshinotented/kotlin/utils/GameRegistry.kt
@@ -7,19 +7,6 @@ import net.minecraft.item.crafting.Ingredient
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fml.common.registry.GameRegistry
 
-operator fun ItemStack.times(amount : Int) {
-	count = amount
-}
-
-operator fun Item.times(amount : Int) = ItemStack(this, amount)
-operator fun Block.times(amount : Int) = ItemStack(this, amount)
-
-fun Item.itemStack(metadata : Int) = ItemStack(this, 1, metadata)
-fun Block.itemStack(metadata : Int) = ItemStack(this, 1, metadata)
-
-val Item.itemStack get() = this * 1
-val Block.itemStack get() = this * 1
-
 fun ItemStack.addShapedRecipe(line0 : String, line1 : String, line2 : String, vararg items : Pair<Char, Any>) {
 	GameRegistry.addShapedRecipe(
 			ResourceLocation(""),
---
+++ src/main/kotlin/com/github/hoshinotented/kotlin/utils/Item.kt
@@ -0,0 +1,18 @@
+package com.github.hoshinotented.kotlin.utils
+
+import net.minecraft.block.Block
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+
+operator fun ItemStack.times(amount : Int) : ItemStack = copy().apply {
+	count = amount
+}
+
+operator fun Item.times(amount : Int) = ItemStack(this, amount)
+operator fun Block.times(amount : Int) = ItemStack(this, amount)
+
+fun Item.itemStack(metadata : Int) = ItemStack(this, 1, metadata)
+fun Block.itemStack(metadata : Int) = ItemStack(this, 1, metadata)
+
+val Item.itemStack get() = this * 1
+val Block.itemStack get() = this * 1
